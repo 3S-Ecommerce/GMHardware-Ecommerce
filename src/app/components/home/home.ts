b3d42213033e41bdd818faaefed978b12e5c7960
PedroGreg <pedrojcgregorio@outlook.com>
2026-02-13 00:55:38 -0300

    feat: adicionando itens no carrinho

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,14 +1,21 @@
 import { Component, inject, signal, OnInit } from '@angular/core';
 import { Product } from '../../core/services/product';
+import { Cart } from '../../core/services/cart';
+
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   private produtoService = inject(Product);
+  cart = inject(Cart);
+  adicionarItem(item: any){
+    this.cart.adicionarItem(item)
+  }
   produtos = signal<any[]>([]);
   ngOnInit(): 
   void{
--- src/app/core/services/cart.ts
+++ src/app/core/services/cart.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, computed } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Cart {
-  
+  private itens = signal<any[]>([]);
+  quantidadeTotal = computed(() => this.itens().length)
+  adicionarItem(item: any){
+    this.itens.update((value) => [...value, item])
+    console.log(this.itens())
+  }
 }
