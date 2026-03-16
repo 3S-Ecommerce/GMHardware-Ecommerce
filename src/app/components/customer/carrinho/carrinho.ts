4d94945f9b601572d60d831bd1aba4f65b8a481f
andreluiz2511 <andreluizxrl8@gmail.com>
2026-03-16 18:57:54 -0300

    changed carrinho typescript

--- src/app/components/customer/carrinho/carrinho.ts
+++ src/app/components/customer/carrinho/carrinho.ts
@@ -8,16 +8,26 @@ import { Component } from '@angular/core';
 })
 export class Carrinho {
 
+  preco = 1199;
   quantidade: number = 1;
+  total = this.preco * this.quantidade;
 
   increase(): void {
     this.quantidade++;
+      this.total = this.preco * this.quantidade;
+
   }
 
   decrease(): void {
     if(this.quantidade > 1){
     this.quantidade--;
+          this.total = this.preco * this.quantidade;
+
   }
+
+  
+
+
 }
 
 }
