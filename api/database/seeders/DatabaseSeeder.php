8ac5deae22941782399d59c4ed150b58382f9af3
PEDRO DE JESUS CAMPOS GREGORIO <pedro.jcgregorio@senacsp.edu.br>
2026-04-08 18:26:50 -0300

    fix: adicionando os seeders do banco de dados no call db:seed

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 
 use App\Models\User;
+use Database\Seeders\AdminSeeder;
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\ProductSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,6 +23,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
             'document' => '12312312312'
+        ]);
+        $this->call([
+            AdminSeeder::class,
+            CategorySeeder::class,
+            ProductSeeder::class
         ]);
     }
 }
