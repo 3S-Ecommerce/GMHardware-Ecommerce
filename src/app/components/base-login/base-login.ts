00c2b06d395f4ca883ab771b59f63ddaaa2abfc5
arthurcavalcantedesouza3@gmail.com <arthurcavalcantedesouza3@gmail.com>
2026-04-11 12:21:56 -0300

    feat: Criei os components de login

---
+++ src/app/components/base-login/base-login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-login',
+  imports: [],
+  templateUrl: './base-login.html',
+  styleUrl: './base-login.scss',
+})
+export class BaseLogin {
+
+}
