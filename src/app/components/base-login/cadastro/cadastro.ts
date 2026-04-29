bfe9512449d0f36f543086b2e5750133ed9897a2
arthurcavalcantedesouza3@gmail.com <arthurcavalcantedesouza3@gmail.com>
2026-04-29 17:52:50 -0300

    test: fazendo teste cadastro

--- src/app/components/base-login/cadastro/cadastro.ts
+++ src/app/components/base-login/cadastro/cadastro.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Auth } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss',
 })
 export class Cadastro {
 
+  private fb = inject(FormBuilder);
+  private authService = inject(Auth);
+
+  formRegister = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  });
+
+  onSubmit() {
+
+    const form = this.formRegister.value;
+
+    if (form.password !== form.confirmPassword) {
+      alert('Senhas não coincidem');
+      return;
+    }
+
+    this.authService.register({
+      name: 'Arthur',
+      email: form.email,
+      password: form.password
+    }).subscribe({
+      next: (res: any) => {
+        console.log('SUCESSO', res);
+        alert('Cadastrado com sucesso!');
+      },
+      error: (err) => {
+        console.error('ERRO', err);
+      }
+    });
+  }
 }
---
+++ src/app/core/services/auth.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+
+  private apiUrl = 'http://127.0.0.1:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  register(data: any) {
+    return this.http.post(`${this.apiUrl}/register`, data);
+  }
+
+  login(data: any) {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
+}
