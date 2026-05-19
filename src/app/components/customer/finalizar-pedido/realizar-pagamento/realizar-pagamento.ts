3b74f9cbd4682713ab392fd3fadb79b500e0f6c6
arthurcavalcantedesouza3@gmail.com <arthurcavalcantedesouza3@gmail.com>
2026-05-18 21:24:49 -0300

    feat: feito a validaçao do tipo de pagamento

--- src/app/components/customer/finalizar-pedido/realizar-pagamento/realizar-pagamento.ts
+++ src/app/components/customer/finalizar-pedido/realizar-pagamento/realizar-pagamento.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckoutService } from '../../../../core/services/checkout.service';
+// Importe os componentes (verifique se os nomes das classes são Pix e Cartao)
+import { Pix } from './pix/pix';
+import { Cartao } from './cartao/cartao';
 
 @Component({
   selector: 'app-realizar-pagamento',
-  imports: [ RouterOutlet ],
+  standalone: true,
+  // Adicionamos os componentes aqui para o Angular poder usar as tags no HTML
+  imports: [CommonModule, Pix, Cartao],
   templateUrl: './realizar-pagamento.html',
   styleUrl: './realizar-pagamento.scss',
 })
 export class RealizarPagamento {
+  private checkoutService = inject(CheckoutService);
 
+  // Criamos uma variável que observa qual método foi escolhido
+  metodo = computed(() => this.checkoutService.paymentMethod());
 }
