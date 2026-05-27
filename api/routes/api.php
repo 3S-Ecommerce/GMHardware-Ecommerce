9e87230259e6810f58b09c3eea537204edac27b1
PedroGreg <pedrojcgregorio@outlook.com>
2026-05-27 01:00:09 -0300

    fix: puxando arquivo paymentController para a API

--- api/routes/api.php
+++ api/routes/api.php
@@ -9,7 +9,25 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PaymentController;
 
+
+// Rota de busca de produtos - DEVE VIR ANTES do Route::apiResource("product")
+Route::get("/products/search", [ProductController::class, "search"]);
+
+Route::apiResource("user", UserController::class);
+
+Route::apiResource("admin", AdminController::class);
+
+Route::apiResource("order" , OrderController::class);
+
+Route::apiResource("category", CategoryController::class);
+
+Route::apiResource("product", ProductController::class);
+
+Route::apiResource("order-items", OrderItemsController::class);
+
+Route::post('/pix', [PaymentController::class, 'createPixPayment']);
 // Route::get('/user/{id}', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
